cdc97b9101bb8515b59396786646ed89ff5ff2b2
pradeep.pp <pradeep.pp@gmail.com>
2020-09-14 11:28:26 -0400

    Initial Add. Closes #1

---
+++ src/test/java/com/teksenz/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teksenz.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
