2bf1a277e4aef386de6ecc12d99627b2c1807d9b
pradeep.pp <pradeep.pp@gmail.com>
2020-09-16 14:55:34 -0400

    Add Product controller. Closes #3

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # shop
+
+Product Endpoints
+POST "/api/v1/products/" - Save a new product
+PUT  "/api/v1/products/" - Update a product
+GET "/api/v1/products/" - Find all products
+GET "/api/v1/products/{id}" - Find a product
+DELETE "/api/v1/products/{id}" - Delete a product
+
+
---
+++ src/test/java/com/teksenz/shop/controllers/ProductControllerTest.java
@@ -0,0 +1,94 @@
+package com.teksenz.shop.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teksenz.shop.domain.Product;
+import com.teksenz.shop.services.ProductService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = ProductController.class)
+class ProductControllerTest {
+    private final String baseUrl = "/api/v1/products/";
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private ProductService productService;
+
+    Product getProduct() {
+        return Product.builder()
+                .name("Sharpie")
+                .description("Highligter pen")
+                .price(1.99F)
+                .build();
+    }
+
+    @Test
+    void findAllProducts() throws Exception {
+        Product product = getProduct();
+        product.setId(UUID.randomUUID());
+        given(productService.findAll()).willReturn(List.of(product));
+        mockMvc.perform(get(baseUrl+product.getId()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findAProduct() throws Exception {
+        Product product = getProduct();
+        product.setId(UUID.randomUUID());
+        given(productService.findById(any())).willReturn(product);
+        mockMvc.perform(get(baseUrl+product.getId()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveNewProduct() throws Exception {
+        Product product = getProduct();
+        product.setId(UUID.randomUUID());
+        given(productService.saveNewProduct(any())).willReturn(product);
+        mockMvc.perform(post(baseUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(getProduct())))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location",baseUrl+product.getId()))
+                .andReturn();
+
+    }
+
+    @Test
+    void updateProduct() throws Exception {
+        Product product = getProduct();
+        product.setId(UUID.randomUUID());
+        given(productService.findById(any())).willReturn(product);
+        mockMvc.perform(put(baseUrl+product.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(product)))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deleteById() throws Exception {
+        Product product = getProduct();
+        product.setId(UUID.randomUUID());
+        given(productService.findById(any())).willReturn(product);
+        mockMvc.perform(delete(baseUrl+product.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+}
